6307d80b5536bd61db767901ce028318ab92b6f7
KwasniakJakub <98922994+KwasniakJakub@users.noreply.github.com>
2022-07-20 13:02:12 +0200

    Add files via upload

---
+++ Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enum
+{
+    //Enum - wyliczenie/typ wyliczeniowy
+    //Warto z nich korzystać gdzie tylko się da 
+    //Jest swego rodzaju zabezpieczeniem 
+    internal class Program
+    {
+        enum PoryRoku : sbyte            //sbyte(lub inny typ zmiennych) oznacza jakiego typy dane ma przechowywać
+        {                               //Wartości się inkrementuja(wystarczy najechać na pory roku)
+            Wiosna = 15, Lato = 25, Jesień = 5, Zima = -5  //Z automatu są uznawane jako stringi
+        }
+        static void Main(string[] args)
+        {
+            //PoryRoku poraRoku = PoryRoku.Zima;
+            PoryRoku poraRoku = (PoryRoku)25;
+            Console.WriteLine((int)poraRoku);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Instrukcje skoku/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Instrukcje_skoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INSTRUKCJE SKOKU
+
+
+            int a = 0;
+        //while (true)
+        //{
+        //    if(a > 10) //Jeśli a dojdzie do 10 to pęta się przerwie za pomocą break;
+        //    {
+        //        break;
+        //    }
+        //    Console.WriteLine(a);
+        //    a++;
+        //}
+        //Console.WriteLine(a);
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    if (i % 2 == 1)
+        //    {
+        //        continue; //Kontunuuj ignorująć liczby parzyste(ignoruj to co jest dalej i przejdź do kolejnego obiegu pętli )
+        //    }
+        //    Console.WriteLine(i);
+        //}
+
+        jeden:          //etykiety
+            Console.WriteLine("Jeden");
+            goto trzy; //Po wykonaniu tej lini idź do etykiery 3 i pomiń reszte
+        dwa:
+            Console.WriteLine("Dwa");
+        trzy:
+            Console.WriteLine("Trzy");
+            goto jeden;
+
+
+            Console.ReadKey();
+        }
+    }
+}
